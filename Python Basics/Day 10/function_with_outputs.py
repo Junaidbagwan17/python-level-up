3158c9cdf19e335d1d42359c47067e4e4090a3fc
Junaid Bagwan <junaidbagwan7058@gmail.com>
2025-12-09 23:54:41 +0530

    Function with Output and return

---
+++ Python Basics/Day 10/function_with_outputs.py
@@ -0,0 +1,18 @@
+# function witn outputs
+
+# create parameters f_name  and l_name
+def format_name(f_name, l_name):# parametesr : f_name and l_name
+    # conv parameters into title case
+    print(f_name.title())
+    print(l_name.title())
+format_name(f_name="virAt", l_name='kOhli')
+
+# storing into var function-title into var
+def format_name(f_name, l_name):# parametesr : f_name and l_name
+    # conv parameters into title case
+    formated_fname = f_name.title()
+    formated_lname= l_name.title()
+
+    print(f"{formated_fname} {formated_lname}")
+format_name(f_name="virAt", l_name='kohLi')
+
