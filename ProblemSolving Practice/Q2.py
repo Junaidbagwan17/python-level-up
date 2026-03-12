3c1ccb479947869bc14627890e9fa4606db6c2e7
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-03-12 19:40:40 +0530

    Sum of two numbers

---
+++ ProblemSolving Practice/Q2.py
@@ -0,0 +1,8 @@
+# TODO: Q 2: Calculate the sum of two numbers entered by the user
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+# SUM of two numbers
+total= num1 + num2
+print(f"{num1} + {num2} = {total}")
