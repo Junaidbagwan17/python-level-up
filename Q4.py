3299805b049792f5dfdac962327808b614032226
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-03-13 19:52:26 +0530

    Delete Q4.py

--- Q4.py
+++
@@ -1,6 +0,0 @@
-# TODO: Q4 Write a Python program to calculate the area of a rectangle given its length and width
-length = int(input("enter the length:"))
-width = int(input("enter the width : "))
-
-area = length * width
-print("area of rectanlge: ", area)
