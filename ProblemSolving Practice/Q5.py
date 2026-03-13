b6b539e4fd6c6681f6d077907752092048f3ae55
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-03-13 19:53:32 +0530

    Greeting Message with Inputs

---
+++ ProblemSolving Practice/Q5.py
@@ -0,0 +1,6 @@
+# TODO: Q6: Create a program that takes a user's name and age as input and prints a greeting message
+
+name = input("enter your name:")
+age = int(input("enter your age:"))
+
+print(f"Hellow {name}!, your age is {age}.")
