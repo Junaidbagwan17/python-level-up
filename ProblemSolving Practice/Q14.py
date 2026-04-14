2175ff296a4993fe92db528a23dd00c07d7a2473
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-04-14 08:37:54 +0530

    Celsius to Kelvin

---
+++ ProblemSolving Practice/Q14.py
@@ -0,0 +1,6 @@
+#TODO: Create a program that takes a temperature in Celsius and converts it to Kelvin
+
+celsius = float(input("enter temprature in celsius:"))
+kelvin = celsius + 273.15
+
+print(f"for celsius {celsius} the kelivin can be {kelvin}")
