c161645474ceb1491ad75449e7cd1de16b128b34
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-04-17 00:14:38 +0530

    checks if a number is positive, negative, or zero

---
+++ ProblemSolving Practice/Q23.py
@@ -0,0 +1,10 @@
+#Todo: Write a program that checks if a given number is positive, negative, or zero.
+
+number =  8
+
+if  number < 0:
+    print("number is negative")
+elif number == 0:
+    print("number is zero")
+else:
+    print("number is positive")
