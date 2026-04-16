3a514bd40fe75f1db6cb182a21ac28d7d324799d
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-04-17 00:21:32 +0530

    Using loop print the first 10 even numbers.

---
+++ ProblemSolving Practice/Q24.py
@@ -0,0 +1,10 @@
+# TODO: Create a loop that prints the first 10 even numbers.
+
+# using %
+for number in range(1 ,21):
+    if number % 2 == 0:
+        print(number)
+
+# using *
+for i in range(1, 11):
+    print(2 * i)
