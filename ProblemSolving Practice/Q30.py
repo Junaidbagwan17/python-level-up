59df0bddc8db07d5e8c0aa097a94ee05c754ffad
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-04-17 19:45:52 +0530

    prints the multiplication table  of a given number

---
+++ ProblemSolving Practice/Q30.py
@@ -0,0 +1,10 @@
+# TODO: Implement a program that prints the multiplication table of a given number
+
+# method 1
+number = 5
+for i in range(1, 11):
+    print(number * i)
+
+# method 2
+for i in range(5,51,5):
+    print(i)
