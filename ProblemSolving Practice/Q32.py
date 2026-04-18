e30c3d12ae8e575c075f02957ebf0ae92c81587d
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-04-18 18:04:40 +0530

    print all prime numbers between 1 and 50

---
+++ ProblemSolving Practice/Q32.py
@@ -0,0 +1,10 @@
+# todo: Create a loop that prints all prime numbers between 1 and 50
+
+for num in range(1,51):
+
+    if num > 1:
+        for i in range(2, num):
+            if num %  i == 0:
+                break
+        else:
+            print(num)
