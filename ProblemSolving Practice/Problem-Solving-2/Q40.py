0067023207b0861b3c90e2bfce105388ec2eec83
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-04-24 01:06:24 +0530

    function that sorts list alphabetically

---
+++ ProblemSolving Practice/Problem-Solving-2/Q40.py
@@ -0,0 +1,6 @@
+#TODO: Create a function that takes a list of strings and returns the list sorted alphabetically
+
+names_list = ["Tata", 'Birla', 'Adani', "Wipro"]
+def sort_alpabets(names):
+    return sorted(names) # , reverse=True or names.sort()
+print(sort_alpabets(names_list))
