162cf9946c87cbb1c30de36b16e30cd423a17ffb
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-04-25 02:04:06 +0530

    counts the number of words in sentence

---
+++ ProblemSolving Practice/Problem-Solving-2/Q48.py
@@ -0,0 +1,6 @@
+# TODO: Write a program that takes a sentence as input and counts the number of words in it
+
+sentence = "Hello How are you"
+words = sentence.split()
+count = len(words)
+print('total words in sentence are', count)
