9d714c2ec2d23f811789513166f200678333467b
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-04-25 01:53:03 +0530

    concatenate a single string separated by spaces

---
+++ ProblemSolving Practice/Problem-Solving-2/Q46.py
@@ -0,0 +1,8 @@
+# todo: Given a list of words, concatenate them into a single string separated by spaces
+
+words = ["Data", "Science", "Powerful"]
+
+sentence = " ".join(words)
+print(sentence)
+
+# sentence = " ".join([i.lower() for i in words])
