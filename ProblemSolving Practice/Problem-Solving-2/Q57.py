eae87584348371b8430bf40f1f054bbb91df6da5
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-05-05 01:04:20 +0530

    Create a list of fruits and add a new fruit to it

---
+++ ProblemSolving Practice/Problem-Solving-2/Q57.py
@@ -0,0 +1,6 @@
+# Todo: Create a list of fruits and add a new fruit to the list
+
+fruits = ["Apple", "Pear", "Orange", "Mango"]
+print("Before",fruits)
+fruits.append("Pineapple")
+print("After", fruits)
