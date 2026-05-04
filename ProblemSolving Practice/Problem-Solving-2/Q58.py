4bab4478bf9c5b9e452980841f728fb6e48d096c
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-05-05 01:09:37 +0530

    Access elements in a tuple using indexing

---
+++ ProblemSolving Practice/Problem-Solving-2/Q58.py
@@ -0,0 +1,12 @@
+#TODO: Access elements in a tuple using indexing
+
+my_tuple = ("Apple", "Pear", "Orange", "Mango")
+print("at index 0:", my_tuple[0])  # Apple
+
+print("at index -1:", my_tuple[-1])  # Mango
+
+print("Reversed tuple:", my_tuple[::-1])  # Apple
+
+print("from pear till last:", my_tuple[1:])
+
+print("from apple to orange:", my_tuple[0: 3]) #+1 always
