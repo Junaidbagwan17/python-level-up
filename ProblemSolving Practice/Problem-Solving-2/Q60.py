73c8ad3f39bd56f37dc32e06ec6d633b3970d7e9
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-05-05 01:26:11 +0530

    smallest and largest number in list

---
+++ ProblemSolving Practice/Problem-Solving-2/Q60.py
@@ -0,0 +1,7 @@
+# todo: Write a program that finds the largest and smallest elements in a list
+from numpy.ma.core import minimum
+
+numbers = [52, 43 , 2, 56, 85, 5, 60]
+
+print("Maximum:",max(numbers))
+print("Minimum:",min(numbers))
