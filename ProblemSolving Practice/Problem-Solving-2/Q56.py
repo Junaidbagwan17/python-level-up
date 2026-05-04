b613c7a89ac8632b17f06b4bc8ae41926b8f812b
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-05-05 01:01:24 +0530

    find the sum and average using built-in functions

---
+++ ProblemSolving Practice/Problem-Solving-2/Q56.py
@@ -0,0 +1,9 @@
+# Todo:  Given a list of numbers, find the sum and average using built-in functions
+
+numbers = [70, 74, 86]
+
+total = sum(numbers)
+n = len(numbers)
+
+average_num = round(total / n)
+print(average_num)
