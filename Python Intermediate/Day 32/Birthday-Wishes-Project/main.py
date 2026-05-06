b456aef476f9379951ea5719a435dc88ddafb766
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-05-07 00:13:00 +0530

    Birthday-Wishese-Project Code

---
+++ Python Intermediate/Day 32/Birthday-Wishes-Project/main.py
@@ -0,0 +1,39 @@
+import pandas
+import smtplib
+import random
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+today_month =  now.month
+today_day = now.day
+
+data = pandas.read_csv("birthdays.csv")
+
+# loop through each row of the csv file
+for index, row in data.iterrows():
+    # check if today's month and day # matches birthday month and birthday day
+    if row["day"]==today_day and row["month"] == today_month:
+        person_name = row["name"]
+        person_email = row["email"]
+
+letter_digit = random.randint(1,3)
+selected_letter = f"letter{letter_digit}.txt"
+
+with open(selected_letter, "r") as letter_file:
+    content = letter_file.read()
+    # print(content)
+
+to_replace = "[NAME]"
+letter_to_sent = content.replace(to_replace, person_name)
+print(letter_to_sent)
+
+email = "ml.technologia2026@gmail.com"
+password = "bhjf dtsl ieaj xgyx"
+
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    connection.starttls()
+    connection.login(user=email, password=password)
+    connection.sendmail(from_addr=email, to_addrs=person_email,
+                        msg=f"Subject:Happy Birthday 🎉\n\n{letter_to_sent}".encode("utf-8"))
+    print("mailsend")
