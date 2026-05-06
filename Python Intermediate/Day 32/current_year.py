48286492ae56ef8296f7e9688ffb025c1e0aeed6
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-05-06 23:55:30 +0530

    understanding_datetime_module

---
+++ Python Intermediate/Day 32/current_year.py
@@ -0,0 +1,12 @@
+import datetime as dt
+
+now = dt.datetime.now() #Module → Class → Method
+
+year = now.year
+print(year)
+
+month = now.month
+print(month)
+
+day_of_week = now.weekday()
+print(day_of_week)
