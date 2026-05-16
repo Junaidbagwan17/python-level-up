243dea47fb39dfa734835c7867da57ad601c2e09
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-05-16 22:05:06 +0530

    Update data_mangager.py

--- Python Intermediate/Day 39/data_mangager.py
+++ Python Intermediate/Day 39/data_mangager.py
@@ -2,7 +2,7 @@ import requests
 
 class DataManager:
     def __init__(self):
-        self.SHEETY_Endpoint = "https://api.sheety.co/4b5e176d3148cc5fcc899a3cb6b134fa/flightDeals/prices"
+        self.SHEETY_Endpoint = "YOUR ENDPOINT"
 
     def get_destination_data(self):
         response = requests.get(url=self.SHEETY_Endpoint)
