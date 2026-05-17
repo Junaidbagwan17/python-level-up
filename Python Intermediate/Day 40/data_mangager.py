a615baea9236d7d79da77f46370b106903011654
Junaid Bagwan <junaidbagwan7058@gmail.com>
2026-05-17 16:23:42 +0530

    added user data

--- Python Intermediate/Day 40/data_mangager.py
+++ Python Intermediate/Day 40/data_mangager.py
@@ -2,8 +2,8 @@ import requests
 
 class DataManager:
     def __init__(self):
-        self.SHEETY_Endpoint = "https://api.sheety.co/4b5e176d3148cc5fcc899a3cb6b134fa/flightDeals/prices"
-        self.USERS_Endpoint = "https://api.sheety.co/4b5e176d3148cc5fcc899a3cb6b134fa/flightDeals/users"
+        self.SHEETY_Endpoint = "https://api.sheety.co/YOUR_KEY/flightDeals/prices"
+        self.USERS_Endpoint = "https://api.sheety.co/YOUR_KEY/flightDeals/users"
 
     def get_destination_data(self):
         response = requests.get(url=self.SHEETY_Endpoint)
